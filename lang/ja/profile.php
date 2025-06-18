2258d513e60967bd195148e1fa8d751a4c900ffe
HondaAkihito <honaki.and.1114@gmail.com>
2025-06-18 17:02:35 +0900

    feat: プロフィール画面のプロフィール情報を日本語化

--- lang/ja/profile.php
+++ lang/ja/profile.php
@@ -4,13 +4,13 @@ return [
     // /views/profile/edit.blade.php
     'profile' => 'プロフィール',
 
-    // // /views/profile/partials/update-profile-information-form.blade.php
-    // 'profile_information' => 'プロフィール情報',
-    // "Update your account's profile information and email address." => 'アカウント名やメールアドレスを更新できます。',
-    // 'name' => '名前',
-    // 'email' => 'メールアドレス',
-    // 'save' => '更新する',
-    // 'saved.' => '更新しました。',
+    // /views/profile/partials/update-profile-information-form.blade.php
+    'profile_information' => 'プロフィール情報',
+    "Update your account's profile information and email address." => 'アカウント名やメールアドレスを更新できます。',
+    'name' => '名前',
+    'email' => 'メールアドレス',
+    'save' => '更新する',
+    'saved.' => '更新しました。',
 
     // // /views/profile/partials/update-password-form.blade.php
     // 'update_password' => 'パスワード更新',
