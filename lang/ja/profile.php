25b7c1c9d18f8853bd95dd45fc6e025d9f259808
HondaAkihito <honaki.and.1114@gmail.com>
2025-06-18 17:05:03 +0900

    feat: プロフィール画面のパスワード更新を日本語化

--- lang/ja/profile.php
+++ lang/ja/profile.php
@@ -12,12 +12,12 @@ return [
     'save' => '更新する',
     'saved.' => '更新しました。',
 
-    // // /views/profile/partials/update-password-form.blade.php
-    // 'update_password' => 'パスワード更新',
-    // 'Ensure your account is using a long, random password to stay secure.' => 'アカウントを安全に保つため、長くてランダムなパスワードを使用してください。',
-    // 'current_password' => '現在のパスワード',
-    // 'new_password' => '新しいパスワード',
-    // 'confirm_password' => '新しいパスワード(再確認)',
+    // /views/profile/partials/update-password-form.blade.php
+    'update_password' => 'パスワード更新',
+    'Ensure your account is using a long, random password to stay secure.' => 'アカウントを安全に保つため、長くてランダムなパスワードを使用してください。',
+    'current_password' => '現在のパスワード',
+    'new_password' => '新しいパスワード',
+    'confirm_password' => '新しいパスワード(再確認)',
 
     // // /views/profile/partials/delete-user-form.blade.php
     // 'delete_account' => 'アカウント削除',
