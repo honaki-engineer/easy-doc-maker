7ecb3ab3f9865cd500366d30dd58cf2eea953a2d
HondaAkihito <honaki.and.1114@gmail.com>
2025-06-19 19:10:24 +0900

    feat: ReceiptSettingServiceの作成|updateReceiptSettingメソッドの作成

--- app/Http/Controllers/ReceiptSettingController.php
+++ app/Http/Controllers/ReceiptSettingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ReceiptSettingService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -25,17 +26,8 @@ class ReceiptSettingController extends Controller
     {
         $receipt_setting = Auth::user()->receiptSettings;
 
-        $receipt_setting->user_id = Auth::id();
-        $receipt_setting->postal_code = $request->postal_code;
-        $receipt_setting->address_line1 = $request->address_line1;
-        $receipt_setting->address_line2 = $request->address_line2;
-        $receipt_setting->issuer_name = $request->issuer_name;
-        $receipt_setting->issuer_number = $request->issuer_number;
-        $receipt_setting->tel_fixed = $request->tel_fixed;
-        $receipt_setting->tel_mobile = $request->tel_mobile;
-        $receipt_setting->responsible_name = $request->responsible_name;
-
-        $receipt_setting->save();
+        // 更新処理
+        ReceiptSettingService::updateReceiptSetting($receipt_setting, $request);
 
         return redirect()
             ->route('receipt_settings.show', compact('receipt_setting'))
---
+++ app/Services/ReceiptSettingService.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Auth;
+
+class ReceiptSettingService
+{
+    // 更新処理
+    public static function updateReceiptSetting($receipt_setting, $request) {
+        $receipt_setting->user_id = Auth::id();
+        $receipt_setting->postal_code = $request->postal_code;
+        $receipt_setting->address_line1 = $request->address_line1;
+        $receipt_setting->address_line2 = $request->address_line2;
+        $receipt_setting->issuer_name = $request->issuer_name;
+        $receipt_setting->issuer_number = $request->issuer_number;
+        $receipt_setting->tel_fixed = $request->tel_fixed;
+        $receipt_setting->tel_mobile = $request->tel_mobile;
+        $receipt_setting->responsible_name = $request->responsible_name;
+
+        $receipt_setting->save();
+    }
+}
+
+?>
