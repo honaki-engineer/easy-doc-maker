cd3daffb02925ea1943ecde53b1db9c294643527
HondaAkihito <honaki.and.1114@gmail.com>
2025-07-22 21:15:12 +0900

    fix: use文の小文字を大文字に修正

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ReceiptRequest;
 use App\Models\PaymentMethod;
-use App\Models\customerName;
+use App\Models\CustomerName;
 use App\Services\ReceiptService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
